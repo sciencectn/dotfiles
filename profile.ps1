1a9d2cbdfbd02bab40d462fa8ac1f0038a535f34
Chris Taylor <1373858+sciencectn@users.noreply.github.com>
2024-02-24 16:36:11 -0800

    Create profile.ps1

---
+++ profile.ps1
@@ -0,0 +1,5 @@
+Import-Module ZLocation
+New-Alias -Name "j" Invoke-ZLocation
+New-Alias -Name "subl" "C:\Program Files\Sublime Text\subl.exe"
+set-psReadLineOption -PredictionViewStyle ListView
+Import-Module git-aliases -DisableNameChecking
