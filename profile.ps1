2d2b294712ac0f23c63bf46efc08011dcd1ac66b
Chris Taylor <1373858+sciencectn@users.noreply.github.com>
2025-01-01 08:45:30 -0800

    begone windows. you're in another repo

--- profile.ps1
+++
@@ -1,6 +0,0 @@
-Import-Module ZLocation
-New-Alias -Name "j" Invoke-ZLocation
-New-Alias -Name "subl" "C:\Program Files\Sublime Text\subl.exe"
-Set-PSReadLineOption -PredictionViewStyle ListView
-Set-PSReadlineKeyHandler -Key Tab -Function MenuComplete
-Import-Module git-aliases -DisableNameChecking
